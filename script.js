2405045874dd305e5ddbe678b6d0f026ff91e0b0
MAGN8TER <magnusbehnke07@gmail.com>
2026-01-08 12:21:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,12 +20,21 @@ function loadNBAGames() {
                 // 1. Create the card container
                 const card = document.createElement("div");
                 card.className = "gameCardUI";
+
+                let gameDate = new Date(game.date).toLocaleDateString('en-US', {
+                    month: 'short',
+                    day: 'numeric',
+                    timeZone: 'UTC' // Forces the browser to show the date the API intended
+                });
                 
                 // 2. Insert 3 divs to fill the 3 grid columns defined in style.css
                 // We add justify-self: center to mimic your 'sport' placeholder alignment
                 card.innerHTML = `
                     <div style="justify-self: center;">${game.visitor}</div>
-                    <div style="justify-self: center;">${game.time}</div>
+                    <div style="text-align: center;">
+                        <div style="font-weight: bold;">${game.time}</div>
+                        <div style="font-size: 0.7em; color: #aaa;">${gameDate}</div>
+                    </div>
                     <div style="justify-self: center;">${game.home}</div>
                 `;
 
